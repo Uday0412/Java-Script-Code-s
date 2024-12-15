139326fc0ea0a06b79263fcdf6d42a3476eb2e6b
uday <145842199+Uday0412@users.noreply.github.com>
2024-12-15 18:19:42 +0530

    Create Generate Fibonacci Sequence.js

---
+++ Practice/function sumOfDigits(num) {     return num.toString().split("").reduce((sum, digit) => sum + parseInt(digit), 0); } console.log(sumOfDigits(1234)); /Generate Fibonacci Sequence.js
@@ -0,0 +1,9 @@
+function fibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+console.log(fibonacci(10));
+// Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
